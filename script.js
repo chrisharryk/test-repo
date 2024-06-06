80343d78e1df410640f54da810d7b1950ac0fb84
Chris <kaychrisharry@gmail.com>
2024-06-06 09:53:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// alert('hey I\'m Javascript');
+
+// let admin, _name;
+
+// _name = 'John';
+
+// admin = _name;
+
+// alert(admin)
+
+/*
+    data types ->
+    1. Number
+    2. BigInt
+    3. String
+    4. Boolean
+    5. null
+    6. undefined
+    7. Object & symbol
+    8. typeof
+*/
